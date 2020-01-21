07e3f50686abfef511a411a9cecab590051e907d
Eino Palomaki <eino.palomaki@icloud.com>
2020-01-21 15:27:33 +0200

    struct to header

--- headers.h
+++ headers.h
@@ -6,7 +6,7 @@
 /*   By: epalomak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 13:17:51 by epalomak          #+#    #+#             */
-/*   Updated: 2020/01/08 13:34:30 by epalomak         ###   ########.fr       */
+/*   Updated: 2020/01/21 15:21:54 by epalomak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "./libft/libft.h"
 
-int			printf(const char * restrict formt, ...)
+typedef struct		s_flags
+{
+	int			hash;
+	int			zero;
+	int			minus;
+	int 		plus;
+	int 		fwidth;
+	int			prec;
+	int			slenght:
+	int			hh;
+	int			h;
+	int			l;
+	int			ll;
+	int			capl;
+	char		specif;
 
+	struct s_flags *next;
+}					t_flags;
+
+int			ft_printf(const char * restrict formt, ...);
+
+#endif
